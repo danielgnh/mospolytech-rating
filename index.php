c3d6e12f8636f89a3eaf19b7d5270ab9516a5f8b
Horman <33636385+dhorman@users.noreply.github.com>
2021-08-12 11:46:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="яjquery-3.6.0.min.js"></script>
+    <script src="jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="main.css">
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.0.1/dist/alpine.js" defer=""></script>
     <link rel="stylesheet" href="https://tailwindui.com/css/components-v2.css">
